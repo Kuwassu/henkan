88c2c4e769cad4907b9004b3a4d372f767706c4a
S <94038358+Kuwax@users.noreply.github.com>
2025-04-05 00:25:41 +0900

    トレイアイコン表示

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,15 @@
+use tray_icon::{TrayIconBuilder, Icon};
+
 fn main() {
-    println!("Hello, world!");
+    let icon = Icon::from_path("kuwassu256.ico", Some((32,32))).unwrap();
+    let tray_icon = TrayIconBuilder::new()
+        .with_tooltip("henkan")
+        .with_icon(icon)
+        .build()
+        .unwrap();
+
+    loop {
+        std::thread::sleep(std::time::Duration::from_secs(60));
+    }
+
 }
