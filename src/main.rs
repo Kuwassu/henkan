0a81731336310781c44cf2b3736ea12e93db2714
S <94038358+Kuwax@users.noreply.github.com>
2025-04-05 00:45:13 +0900

    menu itemが表示できない

--- src/main.rs
+++ src/main.rs
@@ -1,15 +1,19 @@
-use tray_icon::{TrayIconBuilder, Icon};
+use tray_icon::{TrayIconBuilder, menu::{Menu, MenuItem} ,Icon};
 
 fn main() {
+    let menu = Menu::new();
+    let _ = menu.append(&MenuItem::new("終了", true, None));
+    let _ = menu.append(&MenuItem::new("設定", false, None));
+
     let icon = Icon::from_path("kuwassu256.ico", Some((32,32))).unwrap();
     let tray_icon = TrayIconBuilder::new()
-        .with_tooltip("henkan")
         .with_icon(icon)
+        .with_tooltip("henkan")
+        .with_menu(Box::new(menu))
         .build()
         .unwrap();
 
     loop {
         std::thread::sleep(std::time::Duration::from_secs(60));
     }
-
 }
